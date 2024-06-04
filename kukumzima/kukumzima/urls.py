e73761f9c8785843c011bb81ba4c53ea5f6911c8
andreazuu <andre.azu@strathmore.edu>
2024-06-04 16:03:42 +0300

    add customer page

--- kukumzima/kukumzima/urls.py
+++ kukumzima/kukumzima/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("farmers/", include("farmers.urls"))
+    path("farmers/", include("farmers.urls")),
+    path("customers/", include("customers.urls"))
 ]
